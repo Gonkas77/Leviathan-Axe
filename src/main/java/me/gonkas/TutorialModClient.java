1b447a7e9f31c0e316606b08b1c62e8c38e795d6
Gonkas77 <152437712+Gonkas77@users.noreply.github.com>
2024-02-11 01:17:21 +0000

    Initial commit

---
+++ src/main/java/me/gonkas/TutorialModClient.java
@@ -0,0 +1,11 @@
+package me.gonkas;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
